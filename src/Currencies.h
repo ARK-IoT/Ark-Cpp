f021debf9ea9c3afeb92dc4a20dd74003fe5e26d
Simon <sleepdefic1t@users.noreply.github.com>
2017-11-28 18:57:26 -0500

    Create Currencies.h

---
+++ src/Currencies.h
@@ -0,0 +1,14 @@
+
+namespace Currencies {
+    
+    const char* crypto[2] = {
+        "ARK", "BTC"
+    };
+    
+    const char* fiat[15] = {
+        "USD", "AUD", "BRL", "CAD", "CHF",
+        "CNY", "EUR", "GBP", "HKD", "IDR",
+        "INR", "JPY", "KRW", "MXN", "RUB"
+    };
+    
+};
