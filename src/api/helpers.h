f26218f576fc71ece183143488a9697f4ad72e9f
sleepdefic1t <simonmdowney@gmail.com>
2018-02-19 15:14:59 -0800

    update /api/helpers
    remove `Successable`

--- src/api/helpers.h
+++ src/api/helpers.h
@@ -10,30 +10,23 @@ namespace API
 namespace Helpers
 {
 
-int substringCount(const char* str, const char* sub)
-{
-  String string = str;
-  String subString = sub;
-
-  if (subString.length() == 0)
-    return 0;
-  int count = 0;
-  for (size_t offset = string.indexOf(sub);
-       offset != std::string::npos;
-       offset = string.indexOf(sub, offset + subString.length()))
+  int substringCount(const char* str, const char* sub)
   {
-    ++count;
-  }
-  return count;
-};
+    String string = str;
+    String subString = sub;
 
-/*  ==========================================================================  */
+    if (subString.length() == 0)
+      return 0;
+    int count = 0;
+    for (size_t offset = string.indexOf(sub);
+        offset != std::string::npos;
+        offset = string.indexOf(sub, offset + subString.length()))
+    {
+      ++count;
+    }
+    return count;
+  };
 
-struct Successable
-{
-public:
-  bool success;
-};
 };
 };
 };
