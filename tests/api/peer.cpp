4f0b32443c66205f080fc820f848ee82677da04b
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-02-24 12:20:14 -0600

    updated peer test

--- tests/api/peer.cpp
+++ tests/api/peer.cpp
@@ -6,14 +6,14 @@ TEST(peer, test_peer) {
 	ARK::API::Manager _arkManager(ARK::Constants::Networks::Devnet::model);
 
 	const auto peer = _arkManager.peer("167.114.29.55", 4002);
-	ASSERT_STREQ("", peer.ip());
-	ASSERT_EQ(0, peer.port());
-	ASSERT_STREQ("", peer.version());
+	ASSERT_STREQ("167.114.29.55", peer.ip());
+	ASSERT_EQ(4002, peer.port());
+	ASSERT_STREQ("1.1.1", peer.version());
 	ASSERT_EQ(0, peer.errors());
-	ASSERT_STREQ("", peer.os());
-	ASSERT_STREQ("", peer.height());
-	ASSERT_STREQ("", peer.status());
-	ASSERT_EQ(0, peer.delay());
+	ASSERT_STREQ("linux4.4.0-79-generic", peer.os());
+	//ASSERT_STREQ("2679148", peer.height());
+	ASSERT_STREQ("OK", peer.status());
+	ASSERT_EQ(22, peer.delay());
 
 
 	/*  ==================================  */
@@ -30,7 +30,7 @@ TEST(peer, test_peer) {
 
 	/*  ==================================  */
 	const auto version = _arkManager.peerVersion();
-	ASSERT_STREQ("", version.version());
+	ASSERT_STREQ("1.1.1", version.version());
 	ASSERT_STREQ("", version.build());
 
 }
