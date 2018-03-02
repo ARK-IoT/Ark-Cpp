ac89b035633158935b92df162fb47eabf400f560
sleepdefic1t <simonmdowney@gmail.com>
2018-03-01 16:01:36 -0800

    move ark.h

---
+++ src/ark.h
@@ -0,0 +1,14 @@
+/***************************************
+* Ark-Cpp - for the Ark Ecosystem (ark.io)
+* https://github.com/Ark-IoT/Ark-Cpp
+*
+* sleepdeficit(simon@ark.io)
+* MIT | Copyright S. Downey 2017-2018
+***************************************/
+
+#ifndef ARK_H
+#define ARK_H
+
+#include "Ark-Cpp.h"
+
+#endif
