7d047f6d1f2e3f6eb2c64671edade08f139e875f
sleepdefic1t <simonmdowney@gmail.com>
2018-03-01 16:04:36 -0800

    add Ark-Cpp headers

---
+++ src/Ark-Cpp.h
@@ -0,0 +1,24 @@
+/***************************************
+* Ark-Cpp - for the Ark Ecosystem (ark.io)
+* https://github.com/Ark-IoT/Ark-Cpp
+*
+* sleepdeficit(simon@ark.io)
+* MIT | Copyright S. Downey 2017-2018
+***************************************/
+
+#ifndef ARK_CPP_H
+#define ARK_CPP_H
+
+#ifdef __cplusplus
+
+#include <Ark-Cpp.hpp>
+
+using namespace ARK;
+
+#else
+
+#error C++ compiler required. expected file extensions: ".cc" ".cpp"
+
+#endif
+
+#endif
