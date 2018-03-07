e1556311897907b6fb984d05b1e80bde336d24b0
Chris <chrisjohnsonmail@gmail.com>
2018-03-06 21:27:29 -0600

    Removed debugging code

--- src/utilities/http.cpp
+++ src/utilities/http.cpp
@@ -20,19 +20,14 @@ public:
 	HTTP() = default;
 
 	std::string get(const std::string& peer, int port, const std::string& request_str) override {
-		std::cout << "Opening HTTP connection to " << peer << ":" << port << "with request" << std::endl << '\t' << request_str << std::endl;
 		std::ostringstream ss;
 		ss << peer << ":" << port;
 		Poco::Net::HTTPClientSession session(Poco::Net::SocketAddress(ss.str()));
-		std::cout << "HTTPClientSession done" << std::endl;
 		Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, request_str, Poco::Net::HTTPMessage::HTTP_1_1);
-		std::cout << "HTTPRequest done" << std::endl;
 		Poco::Net::HTTPResponse response;
 		try {
 		session.sendRequest(request);
-		std::cout << "HTTPRequest sent" << std::endl;
 		auto& rs = session.receiveResponse(response);
-		std::cout << "HTTPResponse: " << response.getStatus() << " " << response.getReason() << std::endl;
 		if (response.getStatus() == Poco::Net::HTTPResponse::HTTP_OK)
 		{
 			
