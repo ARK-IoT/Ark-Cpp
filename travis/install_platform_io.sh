6bc600b27a187f331de711445d6078fefa8029da
Chris <chrisjohnsonmail@gmail.com>
2018-03-22 15:44:50 -0500

    Convert to AUnit

--- travis/install_platform_io.sh
+++ travis/install_platform_io.sh
@@ -3,5 +3,5 @@ sudo pip install -U platformio
 # update PlatformIO
 platformio update
 
-# install ArduinoJson (64), HttpClient (66), ArduinoUnit (946) libraries
-platformio lib -g install 64 66 946
+# install ArduinoJson (64), HttpClient (66), AUnit (2778) libraries
+platformio lib -g install 64 66 2778
