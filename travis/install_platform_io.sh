e6a10466b044a11583532182718227c11e54560d
Chris <chrisjohnsonmail@gmail.com>
2018-03-22 11:57:30 -0500

    Create install_platform_io.sh

---
+++ travis/install_platform_io.sh
@@ -0,0 +1,7 @@
+# install PlatformIO
+sudo pip install -U platformio
+# update PlatformIO
+platformio update
+
+# install ArduinoJson (64), HttpClient (66), ArduinoUnit (946) libraries
+platformio lib -g install 64 66 946
