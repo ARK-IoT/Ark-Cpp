478dbb2cbd78c3b279748fccab46018606368e61
sleepdefic1t <simonmdowney@gmail.com>
2018-05-03 20:03:55 -0700

    update tests to match API & base updates

--- test/api/peer.cpp
+++ test/api/peer.cpp
@@ -2,9 +2,9 @@
 
 #include "api/api.h"
 
-TEST(api, test_peer) {
-	ARK::API::Manager _arkManager(ARK::Constants::Networks::Devnet::model);
-
+TEST(api, test_peer_peer)
+{
+	ARK::API::Manager _arkManager(ARK::Constants::Networks::Model::Devnet);
 	const auto peer = _arkManager.peer("167.114.29.55", 4002);
 	ASSERT_STREQ("167.114.29.55", peer.ip());
 	ASSERT_EQ(4002, peer.port());
@@ -14,23 +14,23 @@ TEST(api, test_peer) {
 	ASSERT_STRNE("0", peer.height());
 	ASSERT_STREQ("OK", peer.status());
 	ASSERT_NE(0, peer.delay());
-
+}
 
 	/*  ==================================  */
 	/*  ==================================  */
-	/*    BROKEN: fix for large callbacks  */
-	/*    Peers callback is ~10,000 bytes  */
+	/*	BROKEN: fix for large callbacks  */
+	/*	Peers callback is ~10,000 bytes  */
 	//  String peersDescription = _arkManager.peers().getValue();
-	//    Serial.println("peersDescription: ");
-	//    Serial.println(peersDescription);
-	//    Serial.println("\n=====\n");
-	//    delay(50);
+	//	Serial.println("peersDescription: ");
+	//	Serial.println(peersDescription);
+	//	Serial.println("\n=====\n");
 	/*  ==================================  */
 	/*  ==================================  */
 
-	/*  ==================================  */
+TEST(api, test_peer_version)
+{
+	ARK::API::Manager _arkManager(ARK::Constants::Networks::Model::Devnet);
 	const auto version = _arkManager.peerVersion();
 	ASSERT_STREQ("1.1.1", version.version());
 	ASSERT_STREQ("", version.build());
-
 }
