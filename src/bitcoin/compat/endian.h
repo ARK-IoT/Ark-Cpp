0e77598d6c8af8d4ef7b797fc5c09dbf4226ff53
Chris <chrisjohnsonmail@gmail.com>
2018-05-08 08:45:10 -0500

    testing portable endian

--- src/bitcoin/compat/endian.h
+++ src/bitcoin/compat/endian.h
@@ -1,192 +1,118 @@
-// Copyright (c) 2014-2017 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// "License": Public Domain
+// I, Mathias Panzenböck, place this file hereby into the public domain. Use it at your own risk for whatever you like.
+// In case there are jurisdictions that don't support putting things in the public domain you can also consider it to
+// be "dual licensed" under the BSD, MIT and Apache licenses, if you want to. This code is trivial anyway. Consider it
+// an example on how to get the endian conversion functions on different platforms.
 
-#ifndef BITCOIN_COMPAT_ENDIAN_H
-#define BITCOIN_COMPAT_ENDIAN_H
+#ifndef PORTABLE_ENDIAN_H__
+#define PORTABLE_ENDIAN_H__
 
-#include <bitcoin/compat/byteswap.h>
+#if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
 
-#include <cstdint>
+#	define __WINDOWS__
 
-#if defined(HAVE_ENDIAN_H)
-#include <endian.h>
-#elif defined(HAVE_SYS_ENDIAN_H)
-#include <sys/endian.h>
 #endif
 
-#if defined(WORDS_BIGENDIAN)
+#if defined(__linux__) || defined(__CYGWIN__)
 
-#if HAVE_DECL_HTOBE16 == 0
-inline uint16_t htobe16(uint16_t host_16bits)
-{
-    return host_16bits;
-}
-#endif // HAVE_DECL_HTOBE16
+#	include <endian.h>
 
-#if HAVE_DECL_HTOLE16 == 0
-inline uint16_t htole16(uint16_t host_16bits)
-{
-    return bswap_16(host_16bits);
-}
-#endif // HAVE_DECL_HTOLE16
+#elif defined(__APPLE__)
 
-#if HAVE_DECL_BE16TOH == 0
-inline uint16_t be16toh(uint16_t big_endian_16bits)
-{
-    return big_endian_16bits;
-}
-#endif // HAVE_DECL_BE16TOH
+#	include <libkern/OSByteOrder.h>
 
-#if HAVE_DECL_LE16TOH == 0
-inline uint16_t le16toh(uint16_t little_endian_16bits)
-{
-    return bswap_16(little_endian_16bits);
-}
-#endif // HAVE_DECL_LE16TOH
+#	define htobe16(x) OSSwapHostToBigInt16(x)
+#	define htole16(x) OSSwapHostToLittleInt16(x)
+#	define be16toh(x) OSSwapBigToHostInt16(x)
+#	define le16toh(x) OSSwapLittleToHostInt16(x)
+ 
+#	define htobe32(x) OSSwapHostToBigInt32(x)
+#	define htole32(x) OSSwapHostToLittleInt32(x)
+#	define be32toh(x) OSSwapBigToHostInt32(x)
+#	define le32toh(x) OSSwapLittleToHostInt32(x)
+ 
+#	define htobe64(x) OSSwapHostToBigInt64(x)
+#	define htole64(x) OSSwapHostToLittleInt64(x)
+#	define be64toh(x) OSSwapBigToHostInt64(x)
+#	define le64toh(x) OSSwapLittleToHostInt64(x)
 
-#if HAVE_DECL_HTOBE32 == 0
-inline uint32_t htobe32(uint32_t host_32bits)
-{
-    return host_32bits;
-}
-#endif // HAVE_DECL_HTOBE32
+#	define __BYTE_ORDER    BYTE_ORDER
+#	define __BIG_ENDIAN    BIG_ENDIAN
+#	define __LITTLE_ENDIAN LITTLE_ENDIAN
+#	define __PDP_ENDIAN    PDP_ENDIAN
 
-#if HAVE_DECL_HTOLE32 == 0
-inline uint32_t htole32(uint32_t host_32bits)
-{
-    return bswap_32(host_32bits);
-}
-#endif // HAVE_DECL_HTOLE32
+#elif defined(__OpenBSD__)
 
-#if HAVE_DECL_BE32TOH == 0
-inline uint32_t be32toh(uint32_t big_endian_32bits)
-{
-    return big_endian_32bits;
-}
-#endif // HAVE_DECL_BE32TOH
+#	include <sys/endian.h>
 
-#if HAVE_DECL_LE32TOH == 0
-inline uint32_t le32toh(uint32_t little_endian_32bits)
-{
-    return bswap_32(little_endian_32bits);
-}
-#endif // HAVE_DECL_LE32TOH
+#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
 
-#if HAVE_DECL_HTOBE64 == 0
-inline uint64_t htobe64(uint64_t host_64bits)
-{
-    return host_64bits;
-}
-#endif // HAVE_DECL_HTOBE64
+#	include <sys/endian.h>
 
-#if HAVE_DECL_HTOLE64 == 0
-inline uint64_t htole64(uint64_t host_64bits)
-{
-    return bswap_64(host_64bits);
-}
-#endif // HAVE_DECL_HTOLE64
+#	define be16toh(x) betoh16(x)
+#	define le16toh(x) letoh16(x)
 
-#if HAVE_DECL_BE64TOH == 0
-inline uint64_t be64toh(uint64_t big_endian_64bits)
-{
-    return big_endian_64bits;
-}
-#endif // HAVE_DECL_BE64TOH
+#	define be32toh(x) betoh32(x)
+#	define le32toh(x) letoh32(x)
 
-#if HAVE_DECL_LE64TOH == 0
-inline uint64_t le64toh(uint64_t little_endian_64bits)
-{
-    return bswap_64(little_endian_64bits);
-}
-#endif // HAVE_DECL_LE64TOH
+#	define be64toh(x) betoh64(x)
+#	define le64toh(x) letoh64(x)
 
-#else // WORDS_BIGENDIAN
+#elif defined(__WINDOWS__)
 
-#if HAVE_DECL_HTOBE16 == 0
-inline uint16_t htobe16(uint16_t host_16bits)
-{
-    return bswap_16(host_16bits);
-}
-#endif // HAVE_DECL_HTOBE16
+#	include <winsock2.h>
+#	include <sys/param.h>
 
-#if HAVE_DECL_HTOLE16 == 0
-inline uint16_t htole16(uint16_t host_16bits)
-{
-    return host_16bits;
-}
-#endif // HAVE_DECL_HTOLE16
+#	if BYTE_ORDER == LITTLE_ENDIAN
 
-#if HAVE_DECL_BE16TOH == 0
-inline uint16_t be16toh(uint16_t big_endian_16bits)
-{
-    return bswap_16(big_endian_16bits);
-}
-#endif // HAVE_DECL_BE16TOH
+#		define htobe16(x) htons(x)
+#		define htole16(x) (x)
+#		define be16toh(x) ntohs(x)
+#		define le16toh(x) (x)
+ 
+#		define htobe32(x) htonl(x)
+#		define htole32(x) (x)
+#		define be32toh(x) ntohl(x)
+#		define le32toh(x) (x)
+ 
+#		define htobe64(x) htonll(x)
+#		define htole64(x) (x)
+#		define be64toh(x) ntohll(x)
+#		define le64toh(x) (x)
 
-#if HAVE_DECL_LE16TOH == 0
-inline uint16_t le16toh(uint16_t little_endian_16bits)
-{
-    return little_endian_16bits;
-}
-#endif // HAVE_DECL_LE16TOH
+#	elif BYTE_ORDER == BIG_ENDIAN
 
-#if HAVE_DECL_HTOBE32 == 0
-inline uint32_t htobe32(uint32_t host_32bits)
-{
-    return bswap_32(host_32bits);
-}
-#endif // HAVE_DECL_HTOBE32
+		/* that would be xbox 360 */
+#		define htobe16(x) (x)
+#		define htole16(x) __builtin_bswap16(x)
+#		define be16toh(x) (x)
+#		define le16toh(x) __builtin_bswap16(x)
+ 
+#		define htobe32(x) (x)
+#		define htole32(x) __builtin_bswap32(x)
+#		define be32toh(x) (x)
+#		define le32toh(x) __builtin_bswap32(x)
+ 
+#		define htobe64(x) (x)
+#		define htole64(x) __builtin_bswap64(x)
+#		define be64toh(x) (x)
+#		define le64toh(x) __builtin_bswap64(x)
 
-#if HAVE_DECL_HTOLE32 == 0
-inline uint32_t htole32(uint32_t host_32bits)
-{
-    return host_32bits;
-}
-#endif // HAVE_DECL_HTOLE32
+#	else
 
-#if HAVE_DECL_BE32TOH == 0
-inline uint32_t be32toh(uint32_t big_endian_32bits)
-{
-    return bswap_32(big_endian_32bits);
-}
-#endif // HAVE_DECL_BE32TOH
+#		error byte order not supported
 
-#if HAVE_DECL_LE32TOH == 0
-inline uint32_t le32toh(uint32_t little_endian_32bits)
-{
-    return little_endian_32bits;
-}
-#endif // HAVE_DECL_LE32TOH
+#	endif
 
-#if HAVE_DECL_HTOBE64 == 0
-inline uint64_t htobe64(uint64_t host_64bits)
-{
-    return bswap_64(host_64bits);
-}
-#endif // HAVE_DECL_HTOBE64
+#	define __BYTE_ORDER    BYTE_ORDER
+#	define __BIG_ENDIAN    BIG_ENDIAN
+#	define __LITTLE_ENDIAN LITTLE_ENDIAN
+#	define __PDP_ENDIAN    PDP_ENDIAN
 
-#if HAVE_DECL_HTOLE64 == 0
-inline uint64_t htole64(uint64_t host_64bits)
-{
-    return host_64bits;
-}
-#endif // HAVE_DECL_HTOLE64
+#else
 
-#if HAVE_DECL_BE64TOH == 0
-inline uint64_t be64toh(uint64_t big_endian_64bits)
-{
-    return bswap_64(big_endian_64bits);
-}
-#endif // HAVE_DECL_BE64TOH
+#	error platform not supported
 
-#if HAVE_DECL_LE64TOH == 0
-inline uint64_t le64toh(uint64_t little_endian_64bits)
-{
-    return little_endian_64bits;
-}
-#endif // HAVE_DECL_LE64TOH
+#endif
 
-#endif // WORDS_BIGENDIAN
-
-#endif // BITCOIN_COMPAT_ENDIAN_H
+#endif
