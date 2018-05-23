4165db2dd26c2ecad329e656e9329eceae7fdfd5
Chris <chrisjohnsonmail@gmail.com>
2018-05-23 16:50:30 -0500

    Test updating bip39 to not use streams

--- test/crypto/bip39.cpp
+++ test/crypto/bip39.cpp
@@ -3,29 +3,30 @@
 #include "bip39/bip39.h"
 
 #include <string>
+#include <cstring>
 #include <set>
-//#include <sstream>
-/*
+
 TEST(crypto_bip39, generate_mnemonic) {
 	auto passphrase = ARK::Crypto::BIP39::generate_mnemonic(ARK::Crypto::BIP39::language::en, 24);
-	std::istringstream stream(passphrase);
 	std::set<std::string> words;
-	for (std::string word; stream; ) {
-		stream >> word;
-		if (word.empty()) { continue; }
+	auto pch = strtok(passphrase.c_str(), ' ');
+	while (pch != nullptr) {
+		std::string word(pch);
 		ASSERT_TRUE(words.insert(std::move(word)).second);
+		pch = strtok(nullptr, ' ');
 	}
+	
 	ASSERT_EQ(24, words.size());
 
 	passphrase = ARK::Crypto::BIP39::generate_mnemonic(ARK::Crypto::BIP39::language::en, 12);
 	words.clear();
-	stream.str(passphrase);
-	stream.clear();
-	for (std::string word; stream; ) {
-		stream >> word;
-		if (word.empty()) { continue; }
+	
+	pch = strtok(passphrase.c_str(), ' ');
+	while (pch != nullptr) {
+		std::string word(pch);
 		ASSERT_TRUE(words.insert(std::move(word)).second);
+		pch = strtok(nullptr, ' ');
 	}
+	
 	ASSERT_EQ(12, words.size());
 }
-*/
