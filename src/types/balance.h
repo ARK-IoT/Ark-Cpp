d5fc90eff6d81adc21d3dadad9411c9d21398f4f
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-06-27 13:44:24 -0500

    Correct operator bool() by adding missing return

--- src/types/balance.h
+++ src/types/balance.h
@@ -86,7 +86,7 @@ class Balance
 		const char* arktoshi() const { return arktoshi_; }
 		double getValue() const { return atof(ark_); }
 
-		operator bool() const noexcept { arktoshi_[0] == '\0' && ark_[0] == '\0'; }
+		operator bool() const noexcept { return arktoshi_[0] == '\0' && ark_[0] == '\0'; }
 
 		void setArktoshi(const char* const balanceStr) {
 				strncpy(arktoshi_, balanceStr, ARKTOSHI_SIZE);
