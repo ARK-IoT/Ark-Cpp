6ef51e5819f6cfe1f348b61da9d66ac141a9af2f
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-10 23:17:03 -0500

    Update peer tests to not depend on non-static data so they can pass.

--- test/api/peer.cpp
+++ test/api/peer.cpp
@@ -8,7 +8,7 @@ TEST(api, test_peer_peer)
 	const auto peer = _arkManager.peer("167.114.29.55", 4002);
 	ASSERT_STREQ("167.114.29.55", peer.ip());
 	ASSERT_EQ(4002, peer.port());
-	ASSERT_STREQ("1.1.1", peer.version());
+	ASSERT_STRNE("", peer.version());
 	//ASSERT_EQ(0, peer.errors());
 	ASSERT_STREQ("linux4.4.0-79-generic", peer.os());
 	ASSERT_STRNE("0", peer.height());
@@ -31,7 +31,7 @@ TEST(api, test_peer_version)
 {
 	ARK::API::Manager _arkManager(ARK::Constants::Networks::Model::Devnet);
 	const auto version = _arkManager.peerVersion();
-	ASSERT_STREQ("1.1.1", version.version());
+	ASSERT_STRNE("", version.version());
 	ASSERT_STREQ("", version.build());
 }
 
