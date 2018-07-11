ebc29ce30e0f40123e5b4f01eacd07421c38b016
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-11 15:05:32 -0500

    Initial creation stub of postTransaction sample

---
+++ Examples/crypto/postTransaction/postTransaction.ino
@@ -0,0 +1,29 @@
+#include <ark.h>
+
+
+void postTransaction()
+{
+	// Creates a devnet account
+	const auto network_id = ARK::Constants::Networks::Network_ADV::devnet.pubKeyHash;
+
+	// Creates a mainnet account
+	//const auto network_id = ARK::Constants::Networks::Network_ADV::devnet.pubKeyHash;
+	/*
+	// Randomly generate a 12 word passphrase using English words
+	const auto passphrase = ARK::Crypto::BIP39::generate_mnemonic();
+
+	const auto account = ARK::Crypto::create_account(network_id, passphrase);
+	Serial.print("Address: ");
+	Serial.println(account.address().getValue());
+
+	Serial.print("Public Key: ");
+	Serial.println(account.public_key().getValue());*/
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  postTransaction();
+}
+
+void loop() {}
